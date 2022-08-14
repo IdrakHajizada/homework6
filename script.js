f70464b008420cf1c94a1712986d703d89405055
IdrakHajizada <94737790+IdrakHajizada@users.noreply.github.com>
2022-08-15 00:54:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+//Task No 5
+ function Start(){
+    let month = prompt('Ayi daxil et')
+
+    if(month.toLowerCase === 'january'){
+        alert('Bu ayda 31 gun var')
+    }
+    else if(month.toLowerCase = 'may'){
+        alert('Bu ayda 31 gun var')
+
+    }
+    else if(month.toLowerCase = 'july'){
+        alert('Bu ayda 31 gun var')
+
+    }
+    else if(month.toLowerCase = 'march'){
+        alert('Bu ayda 31 gun var')
+
+    }
+    else if(month.toLowerCase = 'october'){
+        alert('Bu ayda 31 gun var')
+
+    }
+    else if(month.toLowerCase = 'august'){
+        alert('Bu ayda 31 gun var')
+
+    }
+    else if(month.toLowerCase = 'december'){
+        alert('Bu ayda 31 gun var')
+
+    }
+    else if(month.toLowerCase = 'february'){
+        alert('Bu ayda 29 gun var')
+    }
+    else if(month.toLowerCase = 'november'){
+        alert('Bu ayda 30 gun var')
+    }
+    else if(month.toLowerCase = 'june'){
+        alert('Bu ayda 30 gun var')
+    }
+    else if(month.toLowerCase = 'april'){
+        alert('Bu ayda 30 gun var')
+    }
+    else if(month.toLowerCase = 'september'){
+        alert('Bu ayda 30 gun var')
+    }
+    else{
+        alert('bele ay yoxdur')
+    }
+    
+    
+
+}
+
+
+
+
